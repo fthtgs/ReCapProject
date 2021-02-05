8757c95a8e210d4616f9dda1e3cc79369c06d55c
FATİH TOĞUŞ <f6820023@std.yildiz.edu.tr>
2021-02-05 23:09:07 +0300

    05.02 - Son istenilen özelliklerin eklenmesi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,23 +11,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //Fonksiyonları kullanabilmek için tanıtımın yapılması
             CarManager carManager = new CarManager(new EfCarDal());
             BrandManager brandManager = new BrandManager(new EfBrandDal());
             ColorManager colorManager = new ColorManager(new EfColorDal());
 
-            Console.WriteLine("Brand Id'si 5 olan arabalar: \nId\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
+            //Markanın ID'sine göre listeleme
+            Console.WriteLine("Marka ID'si 5 olanların listesi:");
+            Console.WriteLine("Id\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
             foreach (var car in carManager.GetCarsByBrandId(5))
             {
                 Console.WriteLine($"{car.CarId}\t{colorManager.GetByColorId(car.ColorId).ColorName}\t\t{brandManager.GetByBrandId(car.BrandId).BrandName}\t\t{car.ModelYear}\t\t{car.DailyPrice}\t\t{car.Descriptions}");
             }
 
-            Console.WriteLine("\n\nColor Id'si 3 olan arabalar: \nId\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
+            //Renk ID'sine göre listeleme
+            Console.WriteLine("\nRenk ID'si 3 olanların listesi:");
+            Console.WriteLine("Id\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
             foreach (var car in carManager.GetCarsByColorId(3))
             {
                 Console.WriteLine($"{car.CarId}\t{colorManager.GetByColorId(car.ColorId).ColorName}\t\t{brandManager.GetByBrandId(car.BrandId).BrandName}\t\t{car.ModelYear}\t\t{car.DailyPrice}\t\t{car.Descriptions}");
             }
-
-            Console.WriteLine("\n\nGünlük fiyat aralığı 300 ile 500 olan arabalar: \nId\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
+            
+            // Fiyat aralığı belirterek listeleme
+            Console.WriteLine("\nGünlük fiyat aralığı 300 ile 500 olan arabalar: \nId\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
             foreach (var car in carManager.GetByDailyPrice(300, 500))
             {
                 Console.WriteLine($"{car.CarId}\t{colorManager.GetByColorId(car.ColorId).ColorName}\t\t{brandManager.GetByBrandId(car.BrandId).BrandName}\t\t{car.ModelYear}\t\t{car.DailyPrice}\t\t{car.Descriptions}");
@@ -35,8 +41,10 @@ namespace ConsoleUI
 
             Console.WriteLine("\n");
 
+            //Yeni araba eklemeyi deneme - Hatalı ekleme
             carManager.Add(new Car { BrandId = 2, ColorId = 2, DailyPrice = -20, ModelYear = 2018, Descriptions = "Otomatik - Dizel - Ekonomi" });
 
+            //Yeni marka eklemeyi deneme - Hatalı ekleme
             brandManager.Add(new Brand { BrandName = "a"});
 
 
