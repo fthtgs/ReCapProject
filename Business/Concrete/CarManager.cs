65678743a112473a2488545a50137c411f9f30f6
FATİH TOĞUŞ <f6820023@std.yildiz.edu.tr>
2021-02-08 13:55:44 +0300

    08.02

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -38,6 +39,16 @@ namespace Business.Concrete
         public List<Car> GetByDailyPrice(decimal min, decimal max)
         {
             return _carDal.GetAll(p => p.DailyPrice >= min && p.DailyPrice <= max);
+        }
+
+        public Car GetById(int carId)
+        {
+            return _carDal.Get(c => c.CarId == carId);
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
         }
 
         public List<Car> GetCars()
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,6 +11,42 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //FirstChange();    // 8. Gün 1. Ödev
+
+            //Add_and_Delete();   // 9. Gün ödevi -- Add ve Delete denemesi
+            CarManager carManager = new CarManager(new EfCarDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine(car.BrandName + "--" + car.ColorName + "--" + car.DailyPrice + "TL");
+            }
+
+        }
+
+        private static void Add_and_Delete()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            Car car1 = new Car { BrandId = 1, ColorId = 1, DailyPrice = 180, ModelYear = 2018, Descriptions = "Otomatik - Dizel - Ekonomik" };
+
+            carManager.Add(car1);
+            foreach (var car in carManager.GetCars())
+            {
+                Console.WriteLine(car.Descriptions);
+            }
+
+
+            carManager.Delete(carManager.GetById(17)); // en son eklendiği için en sondaki araba id'si ile sildim.
+            foreach (var car in carManager.GetCars())
+            {
+                Console.WriteLine(car.Descriptions);
+            }
+        }
+
+        private static void FirstChange()
+        {
             //Fonksiyonları kullanabilmek için tanıtımın yapılması
             CarManager carManager = new CarManager(new EfCarDal());
             BrandManager brandManager = new BrandManager(new EfBrandDal());
@@ -31,7 +67,7 @@ namespace ConsoleUI
             {
                 Console.WriteLine($"{car.CarId}\t{colorManager.GetByColorId(car.ColorId).ColorName}\t\t{brandManager.GetByBrandId(car.BrandId).BrandName}\t\t{car.ModelYear}\t\t{car.DailyPrice}\t\t{car.Descriptions}");
             }
-            
+
             // Fiyat aralığı belirterek listeleme
             Console.WriteLine("\nGünlük fiyat aralığı 300 ile 500 olan arabalar: \nId\tColor Name\tBrand Name\tModel Year\tDaily Price\tDescriptions");
             foreach (var car in carManager.GetByDailyPrice(300, 500))
@@ -45,13 +81,7 @@ namespace ConsoleUI
             carManager.Add(new Car { BrandId = 2, ColorId = 2, DailyPrice = -20, ModelYear = 2018, Descriptions = "Otomatik - Dizel - Ekonomi" });
 
             //Yeni marka eklemeyi deneme - Hatalı ekleme
-            brandManager.Add(new Brand { BrandName = "a"});
-
-
-
-
-
-
+            brandManager.Add(new Brand { BrandName = "a" });
         }
     }
 }
